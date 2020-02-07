ac35d69b71f05131c252c453a94475c203646da2
zrefai <40441317+zrefai@users.noreply.github.com>
2020-02-06 19:40:24 -0600

    Update oufs_lib.h

--- oufs_lib.h
+++ oufs_lib.h
@@ -4,10 +4,8 @@
 
 #define MAX_PATH_LENGTH 200
 
-// PROVIDED
 void oufs_get_environment(char *cwd, char *disk_name);  //ALIVE
 
-// PROJECT 3
 int oufs_format_disk(char  *virtual_disk_name);   //ALIVE
 int oufs_read_inode_by_reference(INODE_REFERENCE i, INODE *inode);  //ALIVE
 int oufs_write_inode_by_reference(INODE_REFERENCE i, INODE *inode);  //ALIVE
@@ -19,7 +17,6 @@ void oufs_clean_directory_entry(DIRECTORY_ENTRY *entry);    //ALIVE
 BLOCK_REFERENCE oufs_allocate_new_block();  //ALIVE
 INODE_REFERENCE oufs_allocate_new_inode();  //ALIVE
 
-// Helper functions to be provided
 INODE_REFERENCE oufs_look_for_inode_from_root(char *path_tokens, INODE_REFERENCE base_inode, char *base_name, int mkdir_flag);  //ALIVE
 INODE_REFERENCE oufs_look_for_inode_not_from_root(char *path, char *cwd, INODE_REFERENCE base_inode, char *base_name, int path_flag, int mkdir_flag);  //ALIVE
 int string_compare(const void *directory_entry_a, const void *directory_entry_b);   //ALIVE
@@ -31,7 +28,6 @@ int clip(char *str, int begin, int len);
 void regular_write(BLOCK_REFERENCE block_reference, INODE_REFERENCE inode_reference, int len, char * buf, int *offset);
 void bleed_write(BLOCK_REFERENCE block_reference, INODE_REFERENCE inode_reference, int len, char * buf, int data_block, int *offset);
 
-// PROJECT 4 ONLY
 INODE_REFERENCE get_specified_entry(BLOCK_REFERENCE base_block, INODE_REFERENCE base_inode, char *base_name);
 OUFILE* oufs_fopen(char *cwd, char *path, char *mode);
 int oufs_fwrite(OUFILE *fp, char * buf, int len);
